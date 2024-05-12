55b20751d4e5ccb9cbb660a044a17f0e784ffd04
Mohamed AbdelAzeem <lieto.mohamed@gmail.com>
2024-05-13 01:53:25 +0300

    [Assignment6 Part1 success]

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -6,12 +6,15 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
+#include <sys/queue.h>
 #include <signal.h>
 #include <unistd.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <fcntl.h>
 #include <stdbool.h>
+#include <time.h>
+#include <pthread.h>
 
 #define BUFFER_SIZE 1024
 #define MAX_IP_LEN INET_ADDRSTRLEN
@@ -21,12 +24,18 @@ int Socketfd;
 const char *Datafilename = "/var/tmp/aesdsocketdata";
 struct addrinfo *servinfo; // will point to the results
 
-int NewSocketfd;
 int data_fd; // data file to save the data received
 
 char client_ip[MAX_IP_LEN]; // to store information about the client's IP address.
 
 char *buffer; // malloc Buffer to receive from the socket
+
+// Define a mutex variable
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+pthread_mutex_t Timer_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_t timer_thread;
+bool ExittimerFlag = 0;
 
 /* Flags for closing unclosed sockets and files */
 typedef struct aesdsocket_flags
@@ -42,9 +51,201 @@ typedef struct aesdsocket_flags
 
 aesdsocket_flags flags = {false, false, false, false, false, false, false};
 
+void cleanup_on_exit();
 /*********************************************************************/
+/******************** Linked List functions ****************************/
+/*********************************************************************/
+
+// Structure for a node in the linked list
+typedef struct ThreadNode
+{
+    pthread_t thread_id;
+    bool completeFlag;
+    SLIST_ENTRY(ThreadNode)
+    entries;
+} ThreadNode;
+
+// Define the list head
+SLIST_HEAD(ThreadListHead, ThreadNode)
+head = SLIST_HEAD_INITIALIZER(head);
+
+int threadCount = 0;
+// Function to insert a new node into the linked list
+void thread_ls_insert(pthread_t thread_id)
+{
+    ThreadNode *new_node = (ThreadNode *)malloc(sizeof(ThreadNode));
+    if (new_node == NULL)
+    {
+        syslog(LOG_ERR, "Thread Memory Allocation Failed: %s\n", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+    new_node->thread_id = thread_id;
+    new_node->completeFlag = false;
+    SLIST_INSERT_HEAD(&head, new_node, entries);
+    threadCount++;
+}
+
+// Function to delete entries from the SLIST if `delete_flag` is 1
+void delete_Completed_threads()
+{
+    ThreadNode *current = SLIST_FIRST(&head); // Start at the head
+    ThreadNode *previous = NULL;              // To track the previous node
+
+    while (current != NULL)
+    {
+        ThreadNode *next = SLIST_NEXT(current, entries); // Keep track of the next node
+
+        if (current->completeFlag == 1)
+        {
+            pthread_join(current->thread_id, NULL); // Join the thread to clean up
+            // Condition to delete the node
+            if (previous == NULL)
+            {
+                // If the node is the first in the list
+                SLIST_REMOVE_HEAD(&head, entries); // Remove the head
+            }
+            else
+            {
+                // If the node is somewhere in the middle or end
+                SLIST_REMOVE(&head, current, ThreadNode, entries); // Remove by reference
+            }
+
+            // Free the memory of the deleted node
+            free(current);
+        }
+        else
+        {
+            // Update previous only if not deleting
+            previous = current;
+        }
+
+        // Move to the next node
+        current = next;
+    }
+}
+
+// Function to print the linked list
+void thread_ls_print()
+{
+    ThreadNode *node;
+
+    if (SLIST_EMPTY(&head))
+    {
+        syslog(LOG_INFO, "The list is empty.\n");
+        return;
+    }
+
+    // Iterate through the list and print each node's information
+    SLIST_FOREACH(node, &head, entries)
+    {
+
+        syslog(LOG_INFO, "Thread ID: %lu, Complete: %s\n",
+               (unsigned long)node->thread_id,
+               node->completeFlag ? "true" : "false");
+    }
+    syslog(LOG_INFO, "****************************************");
+}
+
+// Function to free the entire linked list
+void thread_ls_free()
+{
+    ThreadNode *node, *temp;
+
+    // Iterate through the list
+    node = SLIST_FIRST(&head); // Start from the head
+    while (node != NULL)
+    {
+        temp = SLIST_NEXT(node, entries); // Store reference to the next node
+        free(node);                       // Free the current node
+        node = temp;                      // Move to the next node
+    }
+
+    // Reset the list to an empty state
+    SLIST_INIT(&head);
+}
+
+void join_all_threads()
+{
+    ThreadNode *current;
+
+    // Iterate through the list of threads
+    SLIST_FOREACH(current, &head, entries)
+    {
+        // Join the thread
+        pthread_join(current->thread_id, NULL);
+    }
+}
+/*********************************************************************/
+
+/**************************Timer functions***************************/
+// Function to add a timestamp to the data file
+void add_timestamp()
+{
+
+    time_t current_time;
+    struct tm *time_info;
+    char time_str[128];
+
+    time(&current_time); // Get current system time
+    time_info = localtime(&current_time);
+
+    // Format time according to RFC 2822 (year, month, day, hour, minute, second)
+    strftime(time_str, sizeof(time_str), "timestamp:%a, %d %b %Y %H:%M:%S\n", time_info);
+
+    // Lock mutex to ensure atomic write
+    pthread_mutex_lock(&mutex);
+    syslog(LOG_INFO, "Writing time stamp");
+    // Open the data file in append mode
+    int fd = open(Datafilename, O_WRONLY | O_CREAT | O_APPEND, 0644);
+    if (fd == -1)
+    {
+        perror("Error opening data file");
+        pthread_mutex_unlock(&mutex);
+        cleanup_on_exit();
+        return;
+    }
+
+    // Write the timestamp to the file
+    write(fd, time_str, strlen(time_str));
+
+    // Close the file
+    close(fd);
+
+    // Unlock mutex after writing
+    pthread_mutex_unlock(&mutex);
+}
+
+// Timer thread to add timestamps every 10 seconds
+void *timestamp_timer()
+{
+    while (true)
+    {
+        sleep(10);       // Wait for 10 seconds
+        add_timestamp(); // Append timestamp to the data file
+        pthread_mutex_lock(&Timer_mutex);
+        if (ExittimerFlag)
+        {
+            pthread_mutex_unlock(&Timer_mutex);
+            break; // Exit the loop to terminate the thread
+        }
+        pthread_mutex_unlock(&Timer_mutex);
+    }
+    pthread_exit(NULL);
+}
+
+/*****************************************************************/
 void cleanup_on_exit()
 {
+
+    pthread_mutex_lock(&Timer_mutex);
+    ExittimerFlag = 1;
+    pthread_mutex_unlock(&Timer_mutex);
+    syslog(LOG_INFO, "flags: %d, %d, %d, %d, %d, %d", flags.ClientSocket_flag, flags.ServerSocket_flag, flags.buffer_flag, flags.data_file_flag, flags.deamon_mode_flag, ExittimerFlag);
+
+    join_all_threads();
+
+    thread_ls_free();
+
     if (flags.servinfo_flag)
     {
         freeaddrinfo(servinfo); // free the linked-list
@@ -62,15 +263,6 @@ void cleanup_on_exit()
             exit(1);
         }
     }
-    if (flags.ClientSocket_flag)
-    {
-        if (close(NewSocketfd) == -1)
-        {
-            perror("Error closing client socket");
-            syslog(LOG_ERR, "Error closing client fd");
-            exit(1);
-        }
-    }
     if (flags.data_file_flag)
     {
         if (close(data_fd) == -1)
@@ -81,7 +273,7 @@ void cleanup_on_exit()
         }
     }
     // Delete the file
-    if(access(Datafilename, F_OK) != -1)
+    if (access(Datafilename, F_OK) != -1)
     {
         if (remove(Datafilename) != 0)
         {
@@ -89,7 +281,7 @@ void cleanup_on_exit()
             exit(1);
         }
     }
-
+    pthread_join(timer_thread, NULL);
     if (flags.logs_flag)
     {
         closelog();
@@ -158,6 +350,140 @@ int run_daemon(void)
     return 0;
 }
 
+// Function to handle client connections
+void *client_handler(void *arg)
+{
+
+    pthread_t tid = pthread_self();
+    syslog(LOG_INFO, "Thread : %ld started\n", tid);
+
+    // Lock the mutex before writing to the file
+    syslog(LOG_INFO, "mutex lock \n");
+
+    pthread_mutex_lock(&mutex);
+
+    int NewSocketfd = *((int *)arg);
+    free(arg);
+    // Create thread for each accepted connection
+    buffer = (char *)malloc(sizeof(char) * BUFFER_SIZE); // buffer used to read and write data to and from the client
+    if (buffer == NULL)
+    {
+        syslog(LOG_ERR, "malloc error: %s\n", strerror(errno));
+        cleanup_on_exit();
+        exit(1);
+    }
+    else
+    {
+        syslog(LOG_INFO, "Successfully created buffer\n");
+        flags.buffer_flag = true;
+    }
+
+    // opening a data file (DATA_FILE) in write-only mode with options to create the file if it doesn't exist and to append data to it.
+    data_fd = open(Datafilename, O_RDWR | O_CREAT | O_APPEND, 0644);
+    if (data_fd == -1)
+    {
+        // syslog(LOG_ERR, "open error: %s\n", gai_strerror(status));
+        cleanup_on_exit();
+        syslog(LOG_INFO, "mutex unlock \n");
+        pthread_mutex_unlock(&mutex); // Unlock the mutex before returning
+        pthread_exit(NULL);
+    }
+
+    flags.data_file_flag = true;
+    syslog(LOG_INFO, "Successfully opend data file fd = %d\n", data_fd);
+
+    // Receive data packets
+    int bytes_received = 0;
+    while ((bytes_received = recv(NewSocketfd, buffer, BUFFER_SIZE, 0)) > 0)
+    {
+        write(data_fd, buffer, bytes_received);
+        if (memchr(buffer, '\n', bytes_received) != NULL)
+        {
+            // New line character found
+            break;
+        }
+    }
+
+    if (bytes_received < 0)
+    {
+        syslog(LOG_ERR, "recv error: %s\n", strerror(errno));
+        syslog(LOG_INFO, "mutex unlock \n");
+        pthread_mutex_unlock(&mutex); // Unlock the mutex before returning
+        pthread_exit(NULL);
+    }
+
+    syslog(LOG_INFO, "Received data from %s: %s ( %d )", client_ip, buffer, bytes_received);
+
+    int reposition_status = lseek(data_fd, 0, SEEK_SET); // Reposition the read/write offset of the file to the beginning
+    if (reposition_status == -1)
+    {
+        perror("lseek");
+        syslog(LOG_ERR, "lseek operation failed");
+        cleanup_on_exit();
+        syslog(LOG_INFO, "mutex unlock \n");
+        pthread_mutex_unlock(&mutex); // Unlock the mutex before returning
+        pthread_exit(NULL);
+    }
+
+    memset(buffer, 0, sizeof(char) * BUFFER_SIZE); // clear the buffer
+
+    // reading data from the data file into the buffer in chunks of up to BUFFER_SIZE bytes using the read function
+    int bytes_read = 0;
+
+    while ((bytes_read = read(data_fd, buffer, sizeof(char) * BUFFER_SIZE)) > 0)
+    {
+        // syslog(LOG_INFO, "sending data back to  %s: %s, %d", client_ip, buffer, (int)bytes_received);
+        int sendstatus = send(NewSocketfd, buffer, bytes_read, 0); // sending the data read from the file back to the client
+        if (sendstatus == -1)
+        {
+            perror("send");
+            syslog(LOG_INFO, "mutex unlock \n");
+            pthread_mutex_unlock(&mutex); // Unlock the mutex before returning
+            pthread_exit(NULL);
+        }
+    }
+
+    free(buffer);
+    flags.buffer_flag = false;
+
+    if (close(NewSocketfd) == -1)
+    {
+        perror("client socket fd");
+        syslog(LOG_ERR, "Error closing client fd");
+        syslog(LOG_INFO, "mutex unlock \n");
+        pthread_mutex_unlock(&mutex); // Unlock the mutex before returning
+        pthread_exit(NULL);
+    }
+    flags.ClientSocket_flag = false;
+    if (close(data_fd) == -1)
+    {
+        perror("data file");
+        syslog(LOG_ERR, "Error closing data file");
+        syslog(LOG_INFO, "mutex unlock \n");
+        pthread_mutex_unlock(&mutex); // Unlock the mutex before returning
+        pthread_exit(NULL);
+    }
+    flags.data_file_flag = false;
+    syslog(LOG_INFO, "Closed connection from %s", client_ip);
+
+    syslog(LOG_INFO, "Main mutex unlock \n");
+    pthread_mutex_unlock(&mutex); // Unlock the mutex before returning
+
+    ThreadNode *current;
+    SLIST_FOREACH(current, &head, entries)
+    {
+        if (current->thread_id == tid)
+        {
+            current->completeFlag = true;
+            break;
+        }
+    }
+
+    syslog(LOG_INFO, "Thread : %ld Ended\n", tid);
+    pthread_exit(NULL);
+}
+
+// Main thread
 int main(int argc, char *argv[])
 {
 
@@ -185,10 +511,10 @@ int main(int argc, char *argv[])
     struct addrinfo hints;
 
     memset(&hints, 0, sizeof hints); // make sure the struct is empty
-    hints.ai_family = AF_INET;     //  IPv4 
+    hints.ai_family = AF_INET;       //  IPv4
     hints.ai_socktype = SOCK_STREAM; // TCP stream sockets, SOCK_DGRAM for UDP
     hints.ai_flags = AI_PASSIVE;     // fill in my IP for me
-    hints.ai_protocol = 0;          /* Any protocol */
+    hints.ai_protocol = 0;           /* Any protocol */
 
     if ((status = getaddrinfo(NULL, "9000", &hints, &servinfo)) != 0)
     {
@@ -252,18 +578,36 @@ int main(int argc, char *argv[])
         syslog(LOG_DEBUG, "Daemon is successfully created ");
     }
 
-    // listen to a connection
-    if (listen(Socketfd, 5) != 0)
+    // Create the timer thread
+    syslog(LOG_INFO, "Creating timer thread...");
+    if (pthread_create(&timer_thread, NULL, timestamp_timer, NULL) != 0)
     {
-        perror("listen");
-        syslog(LOG_ERR, "listen error: %s\n", gai_strerror(status));
+        perror("Error creating timer thread");
         cleanup_on_exit();
-        exit(1);
+        exit(EXIT_FAILURE);
     }
-    syslog(LOG_INFO, "Listen on the server socket ");
+    else
+    {
+        pthread_mutex_lock(&Timer_mutex);
+        ExittimerFlag = 0;
+        pthread_mutex_unlock(&Timer_mutex);
+
+        // listen to a connection
+        if (listen(Socketfd, 5) != 0)
+        {
+            perror("listen");
+            syslog(LOG_ERR, "listen error: %s\n", gai_strerror(status));
+            cleanup_on_exit();
+            exit(EXIT_FAILURE);
+        }
+        syslog(LOG_INFO, "Listen on the server socket ");
+    }
 
     while (1)
     {
+
+        delete_Completed_threads();
+
         //  accept the connection
         struct sockaddr_in client_addr;
         unsigned int client_len = sizeof(client_addr);
@@ -282,93 +626,32 @@ int main(int argc, char *argv[])
         // Logs message to the syslog “Accepted connection from xxx” where XXXX is the IP address of the connected client.
         syslog(LOG_INFO, "Accepted connection from %s", client_ip);
 
-        buffer = (char *)malloc(sizeof(char) * BUFFER_SIZE); // buffer used to read and write data to and from the client
-        if (buffer == NULL)
+        // Ensure each thread gets a unique socket descriptor
+        int *thread_socket = malloc(sizeof(int)); // Allocate memory for thread-specific socket descriptor
+        if (!thread_socket)
         {
-            syslog(LOG_ERR, "malloc error: %s\n", gai_strerror(status));
-            cleanup_on_exit();
-            exit(1);
+            syslog(LOG_ERR, "Memory allocation failed");
+            close(NewSocketfd);
+            continue;
+        }
+        *thread_socket = NewSocketfd; // Assign the accepted socket descriptor
+
+        // Create a new thread to handle the client connection
+        pthread_t tid;
+        if (pthread_create(&tid, NULL, client_handler, thread_socket) != 0)
+        {
+            syslog(LOG_ERR, "pthread_create error: %s\n", strerror(errno));
+            close(NewSocketfd);
+            free(thread_socket);
+            continue;
         }
         else
         {
-            syslog(LOG_INFO, "Successfully created buffer\n");
-            flags.buffer_flag = true;
+            // Insert new thread to the linked list
+            thread_ls_insert(tid);
         }
-
-        // opening a data file (DATA_FILE) in write-only mode with options to create the file if it doesn't exist and to append data to it.
-        data_fd = open(Datafilename, O_RDWR | O_CREAT | O_APPEND, 0644);
-        if (data_fd == -1)
-        {
-            syslog(LOG_ERR, "open error: %s\n", gai_strerror(status));
-            cleanup_on_exit();
-            exit(1);
-        }
-        flags.data_file_flag = true;
-        syslog(LOG_INFO, "Successfully opend data file fd = %d\n", data_fd);
-
-        // Receive data packets
-        int bytes_received = 0;
-        while ((bytes_received = recv(NewSocketfd, buffer, BUFFER_SIZE, 0)) > 0)
-        {
-            write(data_fd, buffer, bytes_received);
-            if (memchr(buffer, '\n', bytes_received) != NULL)
-            {
-                // New line character found
-                break;
-            }
-        }
-
-        if (bytes_received < 0)
-        {
-            syslog(LOG_ERR, "recv error: %s\n", gai_strerror(status));
-            continue;
-        }
-
-        // syslog(LOG_INFO, "Received data from %s: %s, %d", client_ip, buffer, (int)bytes_received);
-
-        int reposition_status = lseek(data_fd, 0, SEEK_SET); // Reposition the read/write offset of the file to the beginning
-        if (reposition_status == -1)
-        {
-            perror("lseek");
-            syslog(LOG_ERR, "lseek operation failed");
-            cleanup_on_exit();
-            exit(1);
-        }
-
-        memset(buffer, 0, sizeof(char) * BUFFER_SIZE); // clear the buffer
-
-        // reading data from the data file into the buffer in chunks of up to BUFFER_SIZE bytes using the read function
-        int bytes_read = 0;
-
-        while ((bytes_read = read(data_fd, buffer, sizeof(char) * BUFFER_SIZE)) > 0)
-        {
-            // syslog(LOG_INFO, "sending data back to  %s: %s, %d", client_ip, buffer, (int)bytes_received);
-            int sendstatus = send(NewSocketfd, buffer, bytes_read, 0); // sending the data read from the file back to the client
-            if (sendstatus == -1)
-            {
-                perror("send");
-            }
-        }
-
-        syslog(LOG_INFO, "flags: %d, %d, %d, %d, %d", flags.ClientSocket_flag, flags.ServerSocket_flag, flags.buffer_flag, flags.data_file_flag, flags.deamon_mode_flag);
-
-        free(buffer);
-        flags.buffer_flag = false;
-        if (close(NewSocketfd) == -1)
-        {
-            perror("client socket fd");
-            syslog(LOG_ERR, "Error closing client fd");
-            exit(1);
-        }
-        flags.ClientSocket_flag = false;
-        if (close(data_fd) == -1)
-        {
-            perror("data file");
-            syslog(LOG_ERR, "Error closing data file");
-            exit(1);
-        }
-        flags.data_file_flag = false;
-        syslog(LOG_INFO, "Closed connection from %s", client_ip);
+        // print linkedlist
+        thread_ls_print();
     }
 
     closelog();
